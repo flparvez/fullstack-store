d83cc7602834473cc90f4818cf8b4e310c1c3972
flparvez <flparvez23@gmail.com>
2024-10-14 13:06:30 +0600

    fix responsive

--- src/components/CategorySlide.jsx
+++ src/components/CategorySlide.jsx
@@ -33,8 +33,8 @@ const CategorySlides = () => {
           clickable: true,
         }}
         breakpoints={{
-          320: { slidesPerView: 3, spaceBetween: 10 },
-          480: { slidesPerView: 3, spaceBetween: 10 },
+          320: { slidesPerView: 2, spaceBetween: 10 },
+          480: { slidesPerView: 2, spaceBetween: 10 },
           640: { slidesPerView: 3, spaceBetween: 20 },
           768: { slidesPerView: 3, spaceBetween: 20 },
           1024: { slidesPerView: 4, spaceBetween: 30 },
